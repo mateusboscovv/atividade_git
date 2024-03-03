295d9c311a8d7b265cc4c8ec01f232af92641d3b
mateusboscovv <aananiasduartelima@gmail.com>
2024-03-03 10:02:09 -0300

    Teste
    Novo commit após alteração no windows explorer

---
+++ git/main.cs
@@ -0,0 +1,11 @@
+
+using System;
+class program {
+  static void Main() {
+    string nome = "Mateus";
+    string sala = "TURMA34DS";
+    Console.Write(nome);
+    Console.Write(sala);
+    
+  }
+}
